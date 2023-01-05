f4c471d681223f04b286ec2838966666a5f29a3f
Greg <garnold.805@gmail.com>
2023-01-05 13:53:29 -0500

    initial background and test card svg setup

---
+++ src/components/CardBack.js
@@ -0,0 +1,37 @@
+
+
+function CardBack() {
+    return (
+        <svg
+        width={821.14}
+        height={1145.057}
+        viewBox="0 0 217.26 302.963"
+        xmlns="http://www.w3.org/2000/svg"
+        // {...props}
+      >
+        <g transform="translate(3.06 3.277)">
+          <path
+            style={{
+              fill: "#fff",
+              strokeWidth: 0.261961,
+            }}
+            d="M16.2-3.277C5.6-3.277-3.06 5.385-3.06 15.983v264.443c0 10.598 8.661 19.26 19.26 19.26h178.74c10.597 0 19.26-8.662 19.26-19.26V15.982c0-10.597-8.663-19.26-19.26-19.26z"
+          />
+          <rect
+            style={{
+              fill: "#500",
+              strokeWidth: 0.261961,
+            }}
+            width={207.37}
+            height={293.072}
+            x={1.885}
+            y={1.668}
+            ry={14.315}
+            rx={14.315}
+          />
+        </g>
+      </svg>
+    );
+}
+
+export default CardBack
